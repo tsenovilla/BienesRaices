923093ca03b9f4046876355801ff9deee21a8b01
Tomás Senovilla Polo <tspscgs@gmail.com>
2023-06-15 18:38:16 +0200

    Taking off collating from sql cause it causes problems when loading the tables

--- BienesRaicesDB.sql
+++ BienesRaicesDB.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `BLOG` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `titulo_espanol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `titulo_english` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `titulo_espanol` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `titulo_english` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `fecha` date DEFAULT NULL,
-  `autor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `resumen_espanol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `resumen_english` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `imagen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `texto_espanol` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `texto_english` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `autor` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `resumen_espanol` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `resumen_english` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `imagen` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `texto_espanol` longtext CHARACTER SET utf8mb4,
+  `texto_english` longtext CHARACTER SET utf8mb4,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `CONTACTO` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -25,16 +25,16 @@ CREATE TABLE `CONTACTO` (
   `fecha` date DEFAULT NULL,
   `hora` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `PROPIEDADES` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `titulo_espanol` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `titulo_english` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `titulo_espanol` varchar(45) CHARACTER SET utf8mb4 NOT NULL,
+  `titulo_english` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `precio` decimal(10,2) NOT NULL,
-  `imagen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `descripcion_espanol` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `descripcion_english` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `imagen` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `descripcion_espanol` longtext CHARACTER SET utf8mb4 NOT NULL,
+  `descripcion_english` longtext CHARACTER SET utf8mb4,
   `dormitorios` int DEFAULT NULL,
   `wc` int DEFAULT NULL,
   `aparcamientos` int DEFAULT NULL,
@@ -43,20 +43,20 @@ CREATE TABLE `PROPIEDADES` (
   PRIMARY KEY (`id`),
   KEY `ID_VENDEDOR` (`id_vendedor`),
   CONSTRAINT `propiedades_ibfk_1` FOREIGN KEY (`id_vendedor`) REFERENCES `VENDEDORES` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `USUARIOS` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(50) DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `office_timezone` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `VENDEDORES` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `apellido` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb4 NOT NULL,
+  `apellido` varchar(45) CHARACTER SET utf8mb4 NOT NULL,
+  `email` varchar(20) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
